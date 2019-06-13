0cffd1cf394c6544d3da31d2b7818ddaf76ae111
Oliver Fleckenstein <Oliverfl@live.dk>
2019-06-13 17:37:20 +0200

    Fixes for simulator and script for running system by itself

---
+++ Run-AgentsAndMissionControl.ps1
@@ -0,0 +1,17 @@
+param(
+    # The file to launch the simulator with
+    [Parameter(Mandatory=$true)]
+    [String]
+    $Map,
+    [switch]
+    $Containers,
+    [switch]
+    $Centralized,
+    [String]
+    [ValidateSet('3.0', '2.2')]
+    $DotnetVersion = '3.0'
+)
+
+./Run-Agents.ps1 -Map $Map -Containers:$Containers -Centralized:$Centralized -DotnetVersion $DotnetVersion
+./Run-MissionControl.ps1 -Containers:$Containers -DotnetVersion $DotnetVersion
+
